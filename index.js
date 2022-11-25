ddb30a8f9feb48252e6409519e582c8bd28d676f
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-25 11:51:18 +0300

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+"before all"  ,hook
+function logTwoValues(value1, value2) 
+    console.log(`The two values are ${value1} and ${value2}.`);
+    function sayHelloTo(firstName) {
+        console.log(`Hello, ${firstName}!`);
+      } 
+      function sayHelloTo(firstName = "User") {
+        console.log(`Hello, ${firstName}!`);
+      }
+       
